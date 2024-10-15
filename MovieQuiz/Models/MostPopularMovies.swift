f17890a691733fd00aeb2211b473e1630d6f92e5
tayawarmly <avkozlova8@gmail.com>
2024-10-15 23:50:04 +0300

    не отображается алерт в конце

---
+++ MovieQuiz/Models/MostPopularMovies.swift
@@ -0,0 +1,8 @@
+//
+//  MostPopularMovies.swift
+//  MovieQuiz
+//
+//  Created by Taya on 14.10.2024.
+//
+
+import Foundation
---
+++ MovieQuiz/Services/MoviesLoader.swift
@@ -0,0 +1,8 @@
+//
+//  MoviesLoader.swift
+//  MovieQuiz
+//
+//  Created by Taya on 14.10.2024.
+//
+
+import Foundation
